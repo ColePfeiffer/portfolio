94226e1e252864bf9fa772775b6d92b8ce63a690
T <klav@live.de>
2023-04-06 20:46:09 +0200

    added particles

---
+++ src/boot/Particles.js
@@ -0,0 +1,9 @@
+import { boot } from "quasar/wrappers";
+import Particles from "vue3-particles";
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async ({ app }) => {
+  app.use(Particles);
+  // any other code you want to run during bootstrapping
+});
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,83 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header style="background-color: #f4f4f4">
+    <Particles id="tsparticles" :particlesInit="particlesInit" :particlesLoaded="particlesLoaded" :options="{
+      background: {
+        color: {
+          value: '#0d47a1'
+        }
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: 'push'
+          },
+          onHover: {
+            enable: true,
+            mode: 'repulse'
+          },
+          resize: true
+        },
+        modes: {
+          bubble: {
+            distance: 400,
+            duration: 2,
+            opacity: 0.8,
+            size: 40
+          },
+          push: {
+            quantity: 4
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          }
+        }
+      },
+      particles: {
+        color: {
+          value: '#ffffff'
+        },
+        links: {
+          color: '#ffffff',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1
+        },
+        collisions: {
+          enable: true
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outMode: 'bounce',
+          random: false,
+          speed: 6,
+          straight: false
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800
+          },
+          value: 80
+        },
+        opacity: {
+          value: 0.5
+        },
+        shape: {
+          type: 'circle'
+        },
+        size: {
+          random: true,
+          value: 5
+        }
+      },
+      detectRetina: true
+    }" />
+    <q-header style="background-color: transparent">
       <q-toolbar class="bg-transparent" style="height: 70px">
         <q-tabs no-caps v-model="currentTab" class="text-grey-10 absolute-center">
           <q-tab v-for="tab in firstTabs" :key="tab.label" :name="tab.route" :label="tab.label" @click="navigateTo" />
@@ -21,6 +98,7 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
+import { loadFull } from "tsparticles";
 
 export default defineComponent({
   name: 'MainLayout',
@@ -28,8 +106,18 @@ export default defineComponent({
   setup() {
     const leftDrawerOpen = ref(false)
 
+    const particlesInit = async engine => {
+      await loadFull(engine);
+    };
+
+    const particlesLoaded = async container => {
+      console.log("Particles container loaded", container);
+    };
+
     return {
       currentTab: "home",
+      particlesInit,
+      particlesLoaded,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
