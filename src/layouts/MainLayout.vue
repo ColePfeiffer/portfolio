cdfba612be11954152e4b014cb30588d61235566
T <klav@live.de>
2023-06-22 21:50:44 +0200

    iphone background attempt fix #1

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lhh Lpr lFf" class="particles-container">
+  <q-layout view="lhh Lpr lFf" class="particles-container" style="background-color: black">
     <Particles id="tsparticles" :particlesInit="particlesInit" :particlesLoaded="particlesLoaded" :options="{
       fpsLimit: 60,
       interactivity: {
