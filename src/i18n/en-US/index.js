6f8c077a97e515d1339120061610e3341f6f9a3d
T <klav@live.de>
2023-06-24 11:35:51 +0200

    improved texts a lil

--- src/i18n/en-US/index.js
+++ src/i18n/en-US/index.js
@@ -57,7 +57,7 @@ export default {
   kreideWelt: {
     subtitle: "An Interactive Augmented Reality Game",
     description:
-      'Developed as part of the exhibition "The Digital Plays With The Analogue" during the annual HAW Hamburg Rundgang, KreideWelt aims to bridge the gap between the digital and real worlds through interactive experiences.\nPlayers engage directly with a virtual character and its environment by utilizing raycasts to draw in the real world. By leveraging a digital stylus, players navigate through levels, fostering creativity and problem-solving skills. KreideWelt showcases the potential of AR and VR technology, enhancing exhibitions and providing visitors with unique levels of interactivity.',
+      'Developed as part of the exhibition "The Digital Plays With The Analogue" during the annual HAW Hamburg Rundgang, KreideWelt aims to bridge the gap between the digital and real worlds through interactive experiences.\nPlayers engage directly with a virtual character and its environment by utilizing raycasts to draw in the real world. By leveraging a digital stylus, players navigate through levels, fostering creativity and problem-solving skills. KreideWelt showcases the potential of AR and VR technology and how such games could enhance exhibitions and provide visitors with unique levels of interactivity.',
     features: [
       "Real-Time Raycast Implementation",
       "Unity and Vuforia Integration",
